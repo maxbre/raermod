a02402a912a4708310f66e5b4bfada7c8108e798
maxbre <maxbre67@gmail.com>
2022-05-01 20:00:51 +0000

    initial commit

---
+++ R/mapview_aermod_plt.R
@@ -0,0 +1,49 @@
+#' mapview_aermod_plt
+#'
+#' Create a mapview object from AERMOD.PLT file and export as html and png files
+#'
+#' @param plt string path to the AERMOD plt file
+#' @param epsg number epsg to set crs in the raster object, default epsg 32632
+#' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
+#' @param skip number of rows to skip, leave it as default = 6
+#' @param trans_factor trasformation factor to be applied to values, leave it as default = 1
+#' @return mapview object exported as html and png files
+#' @export
+
+mapview_aermod_plt<-function(plt, name_of_map_layer = 'mapview_layer_name', string_filename = 'export_filename', epsg = 32632, skip = 6,  trans_factor = 1){
+
+  net_id <- x <- y <- average_conc <- NULL
+
+  # call read_aremod_plt
+  tbl <- read_aermod_plt(plt, skip = skip)
+
+  # select data, just grid receptors, remove rows corresponding to discrete receptors, where "net_id" is NA
+  tbl <- tbl %>%
+    tidyr::drop_na(net_id) %>% # this is the line for skipping discrete receptors
+    dplyr::select(x, y, average_conc) %>%
+    dplyr::mutate(average_conc = average_conc * trans_factor) #eventually apply transf_factor
+
+
+  # transform to raster
+  r<-raster::rasterFromXYZ(tbl, crs = epsg)
+
+  # eventually to hardcode the binning in mapview
+  #my_bins<-c(0, 1, 2, 3, 4, 5, 10, 100, round(max(raster::values(r)),0))
+  #my_bins<-pretty(range(raster::values(r)))
+
+  # mapview
+  map <- mapview::mapview(r,
+                          # define the palette
+                          col.regions = grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(8, 'RdYlBu'))),
+                          # here eventually define the binning
+                          #at = my_bins,
+                          na.color ='transparent',
+                          alpha.region= 0.5,
+                          legend.opacity=0.5,
+                          layer.name = name_of_map_layer)
+
+  # export html, png, call function
+  rfunctions::export_mapview(map, string_filename)
+
+}
--- R/read_aermod_plt.R
+++ R/read_aermod_plt.R
@@ -3,21 +3,19 @@
 #' Returns a tibble of the output file AERMOD.PLT
 #'
 #' @param plt a string path to the AERMOD.PLT file
-#' @param skip number of rows to skip, leave as default
+#' @param skip number of rows to skip, leave it as default = 6
 #' @return an object of type tibble
 #' @export
-#' @examples \dontrun{
-#' read_aermod_plt('./AERMOD.PLT')
-#' }
 
 read_aermod_plt <-function(plt, skip = 6){
+
+  path_filename <- NULL
 
   # get columns by positions of empty rows
   col_positions <- readr::fwf_empty(plt, skip = skip)
 
   # get and clean column names
   col_names<-readr::read_fwf(plt, col_positions = col_positions, skip = skip, n_max = 1) %>%
-    dplyr::select(-1) %>%                            # delete useless (empty) column
     dplyr::slice(1) %>%                              # get first row
     purrr::flatten_chr() %>%                         # transform to character vector
     stringr::str_replace_all("[^[:alnum:]]", "_") %>%  # some data cleaning
@@ -28,8 +26,8 @@ read_aermod_plt <-function(plt, skip = 6){
                         id = "path_filename",
                         skip = skip+2, # offset of 2 rows
                         col_positions = col_positions)%>%
-    dplyr::select(-2)%>%  # delete useless (empty) column, the very same as above
-    dplyr::rename_with(~col_names, !"path_filename")
+    dplyr::rename_with(~col_names, !path_filename) %>%
+    dplyr::select(-2)  # delete useless (empty) column
 
   tbl
 
